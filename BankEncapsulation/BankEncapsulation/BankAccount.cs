c7a6fbb61a4a489cd326c3d566daaabaa971fefc
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-19 11:42:00 -0500

    first commit

---
+++ BankEncapsulation/BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        //create a private field of type double named balance with a value of 0
+        private double balance = 0;
+
+        //define a method named Deposit that will accept a double and store that value in the balance field
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        //Define a method named GetBalance that will return the amount stored in the balance field
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
+
---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿//create a new instance of the BankAccount class.
+//Allow the user of the application to Deposit money and retrieve their balance through the console.
+
+using BankEncapsulation;
+
+var rubisAccount = new BankAccount();
+
+Console.WriteLine($"How much would you like to deposit today?");
+
+var amountToDeposit = double.Parse(Console.ReadLine());
+
+rubisAccount.Deposit(amountToDeposit);
+
+Console.WriteLine($"Thanks! Your balance is now: ${rubisAccount.GetBalance()}");
